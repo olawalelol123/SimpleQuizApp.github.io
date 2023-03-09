4f0597b637238d8a29ab7c1308847619b449deb6
SHERIFF OLAWALE <119186019+olawalelol123@users.noreply.github.com>
2023-03-09 12:32:19 -0800

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,181 @@
+export const Questions = [
+    {
+        prompt: "Which planet in our solar system is closest to the sun?",
+         optionA: "Venus",
+         optionB: "Mars",
+         optionC: "Mercury",
+         optionD: "Earth",
+         answer: "C",
+    },
+
+    {
+        prompt: "What is the smallest country in the world by land area?",
+         optionA: "Vatican City",
+         optionB: "Nauru",
+         optionC: "San Marino",
+         optionD: "Monaco",
+         answer: "A",
+    },
+
+    {
+        prompt: "Who painted the Mona Lisa?",
+         optionA: "Vincent van Gogh",
+         optionB: "Leonardo da Vinci",
+         optionC: "Michelangelo",
+         optionD: "Pablo Picasso",
+         answer: "B",
+    },
+
+    {
+        prompt: "What is the capital city of Australia?",
+         optionA: "Sydney",
+         optionB: "Melbourne",
+         optionC: "Brisbane",
+         optionD: "Canberra",
+         answer: "D",
+    },
+
+    {
+        prompt: "In what year did World War II end?",
+         optionA: "1943",
+         optionB: "1945",
+         optionC: "1950",
+         optionD: "1947",
+         answer: "B",
+    },
+
+    {
+        prompt: "Which country gifted the Statue of Liberty to the United States?",
+         optionA: "Italy",
+         optionB: "England",
+         optionC: "France",
+         optionD: "Spain",
+         answer: "C",
+    },
+
+    {
+        prompt: "Which is the largest ocean in the world?",
+         optionA: "Pacific Ocean",
+         optionB: "Indian Ocean",
+         optionC: "Arctic Ocean",
+         optionD: "Atlantic Ocean",
+         answer: "A",
+    },
+
+    {
+        prompt: "Who was the first person to walk on the moon?",
+         optionA: "Buzz Aldrin",
+         optionB: "Michael Collins",
+         optionC: "Yuri Gagarin",
+         optionD: "Neil Armstrong",
+         answer: "D",
+    },
+
+    {
+        prompt: "What is the highest mountain in the world?",
+         optionA: "Mount Everest",
+         optionB: "Mount Denali",
+         optionC: "Mount Aconcagua",
+         optionD: "Mount Kilimanjaro",
+         answer: "A",
+    },
+
+    {
+        prompt: "Who wrote the book 'To Kill a Mockingbird'?",
+         optionA: "William Faulkner",
+         optionB: "Ernest Hemingway",
+         optionC: "Harper Lee",
+         optionD: "F. Scott Fitzgerald",
+         answer: "C",
+    },
+
+    {
+        prompt: "What is the chemical symbol for gold?",
+         optionA: "Ag",
+         optionB:  "Au",
+         optionC: "Fe",
+         optionD: "Cu",
+         answer: "B",
+    },
+
+    {
+        prompt: "Which country is the largest producer of coffee in the world?",
+         optionA: "Colombia",
+         optionB: "Ethiopia",
+         optionC: "Brazil",
+         optionD: "Vietnam",
+         answer: "C",
+    },
+
+    {
+        prompt: "Who directed the movie 'Jaws'?",
+         optionA: "Steven Spielberg",
+         optionB: "George Lucas",
+         optionC: "Francis Ford Coppola",
+         optionD: "Martin Scorsese",
+         answer: "A",
+    },
+
+    {
+        prompt: "What is the currency of Japan?",
+         optionA: "Peso",
+         optionB: "Dollar",
+         optionC: "Euro",
+         optionD: "Yen",
+         answer: "D",
+    },
+
+    {
+        prompt: "Who composed the 'Moonlight Sonata'?",
+         optionA: "Frederic Chopin",
+         optionB: "Johann Sebastian Bach",
+         optionC: "Ludwig van Beethoven",
+         optionD: "Wolfgang Amadeus Mozart",
+         answer: "C",
+    },
+
+    {
+        prompt: "What is the largest organ in the human body?",
+         optionA: "Brain",
+         optionB: "Liver",
+         optionC: "Heart",
+         optionD: "Skin",
+         answer: "D",
+    },
+
+    {
+        prompt: "Which two gases make up the majority of the Earth's atmosphere?",
+         optionA: "Oxygen and nitrogen",
+         optionB: "Helium and neon",
+         optionC: "Argon and oxygen",
+         optionD: "Carbon dioxide and nitrogen",
+         answer: "A",
+    },
+
+    {
+        prompt: "Which Shakespearean play features the characters Romeo and Juliet?",
+         optionA: "Othello",
+         optionB: "Romeo and Juliet",
+         optionC: "Hamlet",
+         optionD: "Macbeth",
+         answer: "B",
+    },
+
+    {
+        prompt: "Who was the first female prime minister of the United Kingdom?",
+         optionA: "Hillary Clinton",
+         optionB: "Theresa May",
+         optionC: "Margaret Thatcher",
+         optionD: "Angela Merkel",
+         answer: "C",
+    },
+
+    {
+        prompt: "What is the capital city of Russia?",
+         optionA: "Moscow",
+         optionB: "Kiev",
+         optionC: "Warsaw",
+         optionD: "St. Petersburg",
+         answer: "A",
+    },
+]
